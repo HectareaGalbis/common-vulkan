6e777a1418417401b2198d428eae8366a7180556
Hectarea1996 <hectometrocuadrado@gmail.com>
2022-04-01 18:16:04 +0200

    Update

--- src/physical-device.lisp
+++ src/physical-device.lisp
@@ -37,6 +37,45 @@
     (equal (cffi:foreign-slot-value properties-ptr '(:struct VkPhysicalDeviceProperties) 'deviceType) type)))
 
 
+;; Returns the available device extensions
+(defun get-available-extensions (vk-physical-device)
+  (cffi:with-foreign-object (extension-count-ptr :uint32)
+    (vkEnumerateDeviceExtensionProperties vk-physical-device (cffi:null-pointer)
+                                          extension-count-ptr (cffi:null-pointer))
+    (let ((extension-count (cffi:mem-ref extension-count-ptr :uint32)))
+      (cffi:with-foreign-object (available-extensions-ptr '(:struct VkExtensionProperties) extension-count)
+        (vkEnumerateDeviceExtensionProperties vk-physical-device (cffi:null-pointer)
+                                              extension-count-ptr available-extensions-ptr)
+        (loop for i from 0 below extension-count
+          collect (cffi:foreign-string-to-lisp (cffi:foreign-slot-value (cffi:mem-aptr available-extensions-ptr
+                                                                                       '(:struct VkExtensionProperties) i)
+                                                                       '(:struct VkExtensionProperties) 'extensionName)))))))
+
+
+;; Checks the availability of device extensions
+(defun check-device-extensions (vk-physical-device required-extensions)
+  (let ((available-extensions (get-available-extensions vk-physical-device)))
+    (loop for required-extension in required-extensions
+      always (loop for available-extension in available-extensions
+               thereis (equal required-extension available-extension)))))
+
+
+;; Returns the family queue properties from a device
+(defun get-family-queues-properties (vk-physical-device)
+  (cffi:with-foreign-object (family-queue-count-ptr :uint32)
+    (vkGetPhysicalDeviceQueueFamilyProperties vk-physical-device family-queue-count-ptr (cffi:null-pointer))
+    (let ((family-queue-count (cffi:mem-ref family-queue-count-ptr :uint32)))
+      (cffi:with-foreign-object (family-queues-properties-ptr '(:struct VkQueueFamilyProperties) family-queue-count)
+        (vkGetPhysicalDeviceQueueFamilyProperties vk-physical-device family-queue-count-ptr family-queues-properties-ptr)
+        (loop for i from 0 below family-queue-count
+          collect (cffi:mem-aptr family-queues-properties-ptr '(:struct VkQueueFamilyProperties) i))))))
+
+
+;; Checks the validity of family queues from a device
+(defun check-queue-family-validity (vk-physical-device required-flags vk-surface)
+  (let ((family-queues-properties (get-family-queues-properties vk-physical-device)))))
+    
+
 
 ;;; -------------------------
 ;;; ---- Public functions ---
