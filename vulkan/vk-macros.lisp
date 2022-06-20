fd98144669303cca3a0debcb6e30543200c45caf
Hectarea <hectometrocuadrado@gmail.com>
2022-06-20 20:54:14 +0200

    Update vk-macros.lisp

--- vulkan/vk-macros.lisp
+++ vulkan/vk-macros.lisp
@@ -12,25 +12,6 @@
   `(logior (ash ,major 22) (ash ,minor 12) ,patch))
 
 
-;; Defines a with macro named name, using a constructor and a destructor
-;; The constructor can receive zero or more arguments and can return one or more values
-;; The destructor must receive 'destructor-arity' arguments. These arguments are the first values the
-;; constructor returns
-;; The resulting macro binds some vars to the results from the constructor. These vars can be fewer than the returned values
-(defmacro defwith (name create destroy &key (destructor-arity 1))
-  (with-gensyms ((var "var") (var-list "var-list") (args "args") (ret-list "ret-list") (body "body"))
-    `(defmacro ,name (,var ,args &body ,body)
-       (with-gensyms ((,ret-list "ret-list"))
-         (let ((,var-list (if (listp ,var)
-                              ,var
-                              (list ,var))))
-           `(let ((,,ret-list (multiple-value-list (,',create ,@,args))))
-              (unwind-protect
-                (multiple-value-bind ,,var-list (values-list ,,ret-list)
-                  ,@,body)
-                (apply #',',destroy (subseq ,,ret-list 0 ,',destructor-arity)))))))))
-
-
 ;; Allocates an object and initialize all its members to zero.
 (defun alloc-vulkan-object (struct-type &optional (count 1))
   (let ((object-ptr (cffi:foreign-alloc struct-type :count count)))
